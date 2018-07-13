7ea296f212966fca0d976d2a60245c8fd52d32d4
McGhee-Jake <jmcghee@csumb.edu>
2018-07-12 22:47:37 -0700

    expanded sql database

--- app/src/main/java/com/example/jmcghee/movementpatterns/MovementDialogFragment.java
+++ app/src/main/java/com/example/jmcghee/movementpatterns/MovementDialogFragment.java
@@ -2,10 +2,20 @@ package com.example.jmcghee.movementpatterns;
 
 import android.app.DialogFragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class MovementDialogFragment extends DialogFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialogfragment_movement, container, false);
+
+        EditText etMovementName = view.findViewById(R.id.et_movement_name);
+
+        String movementName = etMovementName.getText().toString();
+
+        return view;
+    }
 }

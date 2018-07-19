16e396bf5d6d4e53902f9f01ecbc6e0c20914c4d
Mcghee-Jake <jmcghee@csumb.edu>
2018-07-18 17:05:58 -0700

    started adding the button to add a new movement to the sequence

--- app/src/main/java/com/example/jmcghee/movementpatterns/adapters/SequenceAdapter.java
+++ app/src/main/java/com/example/jmcghee/movementpatterns/adapters/SequenceAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,9 +14,11 @@ import com.example.jmcghee.movementpatterns.R;
 
 import java.util.List;
 
-public class SequenceAdapter extends RecyclerView.Adapter<SequenceAdapter.SequenceViewHolder> {
+public class SequenceAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     List<Movement> movementList;
+
+    private final int MOVEMENT = 0, ADD_BTN = 1;
 
     class SequenceViewHolder extends RecyclerView.ViewHolder {
 
@@ -29,32 +32,67 @@ public class SequenceAdapter extends RecyclerView.Adapter<SequenceAdapter.Sequen
         }
     }
 
+    class ExtendSequenceViewHolder extends RecyclerView.ViewHolder {
+
+        ImageButton btnExtendSequence;
+
+        public ExtendSequenceViewHolder(View itemView) {
+            super(itemView);
+            btnExtendSequence = itemView.findViewById(R.id.btn_extend_sequence);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position < movementList.size()) {
+            return MOVEMENT;
+        } else {
+            return ADD_BTN;
+        }
+    }
+
     @NonNull
     @Override
-    public SequenceAdapter.SequenceViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View view = inflater.inflate(R.layout.viewholder_sequence, parent, false);
-        SequenceViewHolder viewHolder = new SequenceViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
+        RecyclerView.ViewHolder viewHolder;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+
+        switch (viewType) {
+            case MOVEMENT:
+                View view1 = inflater.inflate(R.layout.viewholder_sequence, parent, false);
+                viewHolder = new SequenceViewHolder(view1);
+                break;
+            default:
+                View view2 = inflater.inflate(R.layout.viewholder_extend_sequence, parent, false);
+                viewHolder = new ExtendSequenceViewHolder(view2);
+                break;
+        }
         return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull SequenceViewHolder holder, int position) {
-        holder.tvMovementName.setText(movementList.get(position).getName());
-        if (position + 1 == movementList.size()) {
-            holder.downArrow.setVisibility(View.GONE);
-        } else {
-            holder.downArrow.setVisibility(View.VISIBLE);
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        switch (holder.getItemViewType()) {
+            case MOVEMENT:
+                SequenceViewHolder sequenceViewHolder = (SequenceViewHolder) holder;
+                sequenceViewHolder.tvMovementName.setText(movementList.get(position).getName());
+                if (position + 1 == movementList.size()) {
+                    sequenceViewHolder.downArrow.setVisibility(View.GONE);
+                } else {
+                    sequenceViewHolder.downArrow.setVisibility(View.VISIBLE);
+                }
+                break;
         }
     }
+
 
     @Override
     public int getItemCount() {
         if (movementList == null) {
             return 0;
         } else {
-            return movementList.size();
+            return movementList.size() + 1;
         }
     }
 
